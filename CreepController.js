3ac4272457028be0f12c0362a2cd0c4041fd07c7
Peter Maidens <p.m.maidens@gmail.com>
2015-06-16 23:24:53 -0600

    rename creep files to add creep.*

--- CreepController.js
+++ CreepController.js
@@ -1,10 +1,10 @@
 module.exports = {
 
     roleControllerMap: {
-        Harvester: require("Harvester"),
-        Enforcer: require("Enforcer"),
-        Builder: require("Builder"),
-        Ranger: require("Ranger")
+        Harvester: require("creep.Harvester"),
+        Enforcer: require("creep.Enforcer"),
+        Builder: require("creep.Builder"),
+        Ranger: require("creep.Ranger")
     },
     
     decide: function(creep) {
