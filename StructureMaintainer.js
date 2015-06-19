5a013843ad50b63213116aba763888cd08b18fab
Peter Maidens <Peter_Maidens@intuit.com>
2015-06-19 12:07:59 -0600

    Rework Structure Maintainer

--- StructureMaintainer.js
+++ StructureMaintainer.js
@@ -1,14 +1,11 @@
 module.exports = function() {
     console.log("Maintaining Structures... " + Game.getUsedCpu());
-    Object.keys(Game.structures).forEach(function(id) {
+/*    Object.keys(Game.structures).forEach(function(id) {
         var structure = Game.getObjectById(id);
-        
-        console.log(structure.structureType + " " + structure.hits + ":" + structure.hitsMax);
+
         if(structure.hits < (structure.hitsMax / 2) && Memory.repairList.indexOf(id) === -1) {
-            console.log(structure.structureType + " " + structure.id + " added.");
             Memory.repairList.push(id);
         } else {
-            console.log(structure.structureType + " " + structure.hits + ":" + structure.hitsMax + " index:" + Memory.repairList.indexOf(id));
             var index = Memory.repairList.indexOf(id);
             if(index > -1 && structure.hits >= (structure.hitsMax / 2)) {
                 Memory.repairList.splice(index, 1);
@@ -18,5 +15,34 @@ module.exports = function() {
             return Game.getObjectById(b).hits - Game.getObjectById(a).hits;
         });
     });
+*/
+    Object.keys(Game.rooms).forEach(function(roomName) {
+        var room = Game.rooms[roomName];
+        var newCandidates = room.find(FIND_STRUCTURES, {
+            filter: function(structure) {
+                return structure.hits < (structure.hitsMax / 2);
+            }
+        });
+
+        Memory.repairList.forEach(function (id) {
+            var structure = Game.getObjectById(id);
+
+            if(structure.hits >= structure.hitsMax*0.75) {
+                Memory.repairList.splice(Memory.repairList.indexOf(id), 1);
+            }
+        })
+
+        newCandidates.forEach(function (structure) {
+            if(Memory.repairList.indexOf(structure.id) === -1) {
+                Memory.repairList.push(structure.id);
+            }
+        });
+
+    });
+
+    /*Memory.repairList = Memory.repairList.sort(function(a, b) {
+        return Game.getObjectById(b).hits - Game.getObjectById(a).hits;
+    });*/
+
     console.log("Finished " + Game.getUsedCpu());
 }
