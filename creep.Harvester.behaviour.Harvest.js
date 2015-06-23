465fe8a992b6a277f0371fe175309b68688c77fe
Peter Maidens <Peter_Maidens@intuit.com>
2015-06-22 22:10:22 -0600

    Add checks for currentTarget defined

--- creep.Harvester.behaviour.Harvest.js
+++ creep.Harvester.behaviour.Harvest.js
@@ -1,15 +1,17 @@
 module.exports = function (creep) {
 	// Aquire target
-	if(creep.memory.currentTarget.ticksToRegeneration === undefined || !creep.memory.currentTarget.energy || !creep.memory.movement.path.length) {
+	if(!creep.memory.currentTarget || creep.memory.currentTarget.ticksToRegeneration === undefined || !creep.memory.currentTarget.energy || !creep.memory.movement.path.length) {
 		creep.memory.currentTarget = creep.pos.findClosest(FIND_SOURCES_ACTIVE, {
     	    algorithm: "astar"
 	    });
 	}
 
 	// Execute on target
-	if(creep.pos.isNearTo(creep.memory.currentTarget.pos.x, creep.memory.currentTarget.pos.y)) {
-		creep.harvest(Game.getObjectById(creep.memory.currentTarget.id));
-	} else {
-		creep.advMove(creep.memory.currentTarget);
+	if(creep.memory.currentTarget) {
+		if(creep.pos.isNearTo(creep.memory.currentTarget.pos.x, creep.memory.currentTarget.pos.y)) {
+			creep.harvest(Game.getObjectById(creep.memory.currentTarget.id));
+		} else {
+			creep.advMove(creep.memory.currentTarget);
+		}
 	}
 };
