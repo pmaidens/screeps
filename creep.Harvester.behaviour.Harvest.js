a5cf892c114ad9972d45b93e8839f761980301e8
Peter Maidens <Peter_Maidens@intuit.com>
2015-06-22 20:15:36 -0600

    Add advanced movement call to Harvester

--- creep.Harvester.behaviour.Harvest.js
+++ creep.Harvester.behaviour.Harvest.js
@@ -10,18 +10,6 @@ module.exports = function (creep) {
 	if(creep.pos.isNearTo(creep.memory.currentTarget.pos.x, creep.memory.currentTarget.pos.y)) {
 		creep.harvest(Game.getObjectById(creep.memory.currentTarget.id));
 	} else {
-		if(!creep.memory.movement.path.length || creep.memory.currentTarget.pos !== creep.memory.movement.targetPos || JSON.stringify(creep.pos) === creep.memory.movement.lastPos || creep.memory.movement.step >= creep.memory.movement.lastCalc/2) {
-			creep.memory.movement = {
-				path: creep.pos.findPathTo(creep.memory.currentTarget.pos.x, creep.memory.currentTarget.pos.y),
-				step: 0,
-				lastPos: creep.pos,
-				lastCalc: 0,
-				targetPos: creep.memory.currentTarget.pos
-			};
-			creep.memory.movement.lastCalc = creep.memory.movement.path.length;
-		}
-		creep.memory.movement.lastPos = creep.pos;
-		creep.move(creep.memory.movement.path[creep.memory.movement.step].direction);
-		creep.memory.movement.step = creep.memory.movement.step + 1;
+		creep.advMove(creep.memory.currentTarget);
 	}
 };
