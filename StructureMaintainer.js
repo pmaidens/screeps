e7f526820de6a37482f8145ccf635c659e6ce104
Peter Maidens <Peter_Maidens@intuit.com>
2015-06-26 11:44:50 -0600

    Refactor Structure Maintainer

--- StructureMaintainer.js
+++ StructureMaintainer.js
@@ -1,40 +1,36 @@
 module.exports = function() {
     console.log("Maintaining Structures... " + Game.getUsedCpu());
-    Object.keys(Game.rooms).forEach(function(roomName) {
-        var room = Game.rooms[roomName];
-        var newCandidates = [],
-            links = [],
-            spawns = [];
-        room.find(FIND_STRUCTURES, {
-            filter: function(structure) {
-                if(structure.hits < (structure.hitsMax / 2)) {
-                    newCandidates.push(structure);
-                } else if(structure.structureType === STRUCTURE_LINK) {
-                    links.push(structure.id);
-                } else if(structure.structureType === STRUCTURE_SPAWN) {
-                    links.push(structure.name);
-                }
-                return false;
-            }
-        });
+    var room = Game.rooms[roomName];
+    var newCandidates = [],
+        links = [],
+        spawns = [];
+    Object.keys(Game.structures).forEach(function(structureId) {
+        structure = Game.structures[structureId];
+        if(structure.hits < (structure.hitsMax / 2)) {
+            newCandidates.push(structure);
+        }
+        if(structure.structureType === STRUCTURE_LINK) {
+            links.push(structure.id);
+        } else if(structure.structureType === STRUCTURE_SPAWN) {
+            links.push(structure.name);
+        }
+    });
 
-        Memory.repairList.forEach(function (id) {
-            var structure = Game.getObjectById(id);
+    Memory.repairList.forEach(function (id) {
+        var structure = Game.getObjectById(id);
 
-            if(!structure || structure.hits >= structure.hitsMax*0.75) {
-                Memory.repairList.splice(Memory.repairList.indexOf(id), 1);
-            }
-        });
+        if(!structure || structure.hits >= structure.hitsMax*0.75) {
+            Memory.repairList.splice(Memory.repairList.indexOf(id), 1);
+        }
+    });
 
-        Memory.linkList = links;
-        Memory.spawnList = spawns;
+    Memory.linkList = links;
+    Memory.spawnList = spawns;
 
-        newCandidates.forEach(function (structure) {
-            if(Memory.repairList.indexOf(structure.id) === -1) {
-                Memory.repairList.push(structure.id);
-            }
-        });
-
+    newCandidates.forEach(function (structure) {
+        if(Memory.repairList.indexOf(structure.id) === -1) {
+            Memory.repairList.push(structure.id);
+        }
     });
     console.log("Finished " + Game.getUsedCpu());
 };
