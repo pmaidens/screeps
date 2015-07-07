5796448830bda5eb4a53263e272aaf795e8b54f2
Peter Maidens <Peter_Maidens@intuit.com>
2015-07-07 14:21:37 -0600

    Try using travis token hardcoded

--- .travis.build.sh
+++ .travis.build.sh
@@ -8,4 +8,4 @@ git config user.email "pmaidens@ualberta.ca"
 git add -f ./build
 git commit -m "Travis Build - Deploying to build folder"
 
-git push --force --quiet "https://${GH_TOKEN}@${GH_REF}" master
+git push --force --quiet "https://NjpzCqapfKVXXBQ29BJH@github.com/pmaidens/screeps.git" master
