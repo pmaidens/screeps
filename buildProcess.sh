1320447250097c942ea054f14b27fbaa2bd3b807
Peter Maidens <Peter_Maidens@intuit.com>
2015-07-07 13:28:58 -0600

    Fixes #11 - Remove the extra logging

--- buildProcess.sh
+++ buildProcess.sh
@@ -12,13 +12,9 @@ mkdir build
 for f in $(find ./src -name '*.js')
 do
   echo "[INFO] Processing $f file..."
-  echo $f
 
   stringSub=${f//$stringSlash/$stringDot}
-  echo $stringSub
   stringFinal=${stringSub#..src.}
-  echo $stringFinal
-  echo build/$stringFinal
   cp $f $PWD/build/$stringFinal
 
 done
