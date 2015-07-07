649d3fd9f2f73294f9dbb3a2f0e9782888d023ff
Peter Maidens <Peter_Maidens@intuit.com>
2015-07-07 15:56:28 -0600

    Fixing Error Logging

--- .travis.build.sh
+++ .travis.build.sh
@@ -10,15 +10,11 @@ git config user.email "peter_maidens@intuit.com"
 git add -f ./build
 
 gitStatus=$(git status --porcelain)
-echo $gitStatus
 gitCount=${#gitStatus}
-echo $gitCount
 if [[ $gitCount > 0 ]]; then
     git commit -m "Travis Build - Deploying to build folder"
-
-
-    echo pushing...
-    gitStatus= git status --porcelain
-    echo gitStatus
-    git push --force --quiet "https://${GH_TOKEN}@${GH_REF}" master
+    echo [INFO] Pushing changes...
+    git push --force "https://${GH_TOKEN}@${GH_REF}" master
+else
+    echo [INFO] No new changes to push.
 fi
