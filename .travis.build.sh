2a43ee77e19d1c246aad526bf1dfea1f371255a1
Peter Maidens <Peter_Maidens@intuit.com>
2015-07-07 15:27:56 -0600

    Making push not quiet

--- .travis.build.sh
+++ .travis.build.sh
@@ -8,4 +8,5 @@ git config user.email "peter_maidens@intuit.com"
 git add -f ./build
 git commit -m "Travis Build - Deploying to build folder"
 
-git push --force --quiet "https://${GH_TOKEN}@${GH_REF}" master
+echo pushing...
+git push --force "https://${GH_TOKEN}@${GH_REF}" master
