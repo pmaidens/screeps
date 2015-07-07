e6e0999f71aa5836dd9cfe6b5cae0bfd95ce8551
Peter Maidens <Peter_Maidens@intuit.com>
2015-07-07 15:25:53 -0600

    Try to get travis working again

--- .travis.build.sh
+++ .travis.build.sh
@@ -2,10 +2,10 @@ set -e
 
 ./buildProcess.sh
 
-git config user.name "Travis CI"
-git config user.email "pmaidens@ualberta.ca"
+git config user.name "Travis-CI"
+git config user.email "peter_maidens@intuit.com"
 
 git add -f ./build
 git commit -m "Travis Build - Deploying to build folder"
 
-git push --force --quiet "https://NjpzCqapfKVXXBQ29BJH@github.com/pmaidens/screeps.git" master
+git push --force --quiet "https://${GH_TOKEN}@${GH_REF}" master
