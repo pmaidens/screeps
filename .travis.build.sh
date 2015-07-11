40e99194d7bb1451fc3b3d7774a9ca9142c2064d
Peter Maidens <Peter_Maidens@intuit.com>
2015-07-10 22:13:43 -0600

    Change build process messaging

--- .travis.build.sh
+++ .travis.build.sh
@@ -13,8 +13,10 @@ gitStatus=$(git status --porcelain)
 gitCount=${#gitStatus}
 if [[ $gitCount > 0 ]]; then
     git commit -m "Travis Build - Deploying to build folder [ci skip]"
+    echo
     echo [INFO] Pushing changes...
     git push --force "https://${GH_TOKEN}@${GH_REF}" $TRAVIS_BRANCH
+    echo [INFO] Changes pushed.
 else
     echo
     echo [INFO] No new changes to push.
--- buildProcess.sh
+++ buildProcess.sh
@@ -9,12 +9,19 @@ stringDot="."
 rm -rf ./build
 mkdir build
 
+echo
+echo "[INFO] Starting build process..."
+echo
+
 for f in $(find ./src -name '*.js')
 do
-  echo "[INFO] Processing $f file..."
+  echo "[INFO] Processing $f"
 
   stringSub=${f//$stringSlash/$stringDot}
   stringFinal=${stringSub#..src.}
   cp $f $PWD/build/$stringFinal
 
 done
+
+echo
+echo "[INFO] File processing complete."
