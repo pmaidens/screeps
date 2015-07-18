2aaaf1ac84d7f5bacb5138992bffbeec0acec741
Travis-CI <peter_maidens@intuit.com>
2015-07-18 17:56:53 +0000

    Travis Build - Deploying to build folder [ci skip]

--- build/RoomMaintainer.js
+++ build/RoomMaintainer.js
@@ -1,15 +1,15 @@
 module.exports = function RoomMaintainer() {
     return {
         updateRooms: function () {
-            Object.keys(Game.rooms).forEach(function (room) {
-                this.maintainRoom(room);
-            });
+            Object.keys(Game.rooms).forEach(function (roomName) {
+                this.maintainRoom(Game.rooms[roomName]);
+            }.bind(this));
         },
 
         maintainRoom: function (room) {
             room.find(FIND_SOURCES).forEach(function (source) {
-                source.memory = source.memory || {};
-            });
+                Memory.sources[source.id] = Memory.sources[source.id] || {};
+            }.bind(this));
         },
 
         newRoom: function (room) {
