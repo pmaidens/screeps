8e1093637aecd880f8f7d3941347975487010132
Travis-CI <peter_maidens@intuit.com>
2015-07-23 17:59:01 +0000

    Travis Build - Deploying to build folder [ci skip]

--- build/StructureMaintainer.js
+++ build/StructureMaintainer.js
@@ -12,13 +12,15 @@ module.exports = function() {
 
     var newCandidates = [],
         links = [],
-        spawns = [];
+        extensions = [];
     structures.forEach(function(structure) {
         if(structure.hits < (structure.hitsMax / 2)) {
             newCandidates.push(structure);
         }
         if(structure.structureType === STRUCTURE_LINK) {
             links.push(structure.id);
+        } else if(structure.structureType === STRUCTURE_EXTENSION) {
+            extensions.push(structure.id);
         }
     });
 
@@ -32,6 +34,7 @@ module.exports = function() {
 
     Memory.linkList = links;
     Memory.spawnList = Game.spawns;
+    Memory.extensionList = extensions;
 
     newCandidates.forEach(function (structure) {
         if(Memory.repairList.indexOf(structure.id) === -1) {
