36fe16bda0857f2f5e88be8473099a04f6b0c07d
Travis-CI <peter_maidens@intuit.com>
2015-08-16 21:35:55 +0000

    Travis Build - Deploying to build folder [ci skip]

--- build/main.js
+++ build/main.js
@@ -1,40 +1,64 @@
-Room.prototype.getLOIs = function() {
-    var lois = this.memory.sources || [];
-    if(this.controller) {
-        lois.push(this.controller.id);
-    }
-    return lois;
-};
+SetupPrototypes();
 
-Object.keys(Game.rooms).forEach(function (roomName) {
-    var room = Game.rooms[roomName];
+SetupRooms();
 
-    if (!room.memory.calcComplete || Memory.recalcRooms) {
-        // Do one time room calculations here
+SpawnCreeps();
 
-        if(!room.memory.sources) {
-            room.memory.sources = room.find(FIND_SOURCES).map(function (sourceObject) {
-                return sourceObject.id;
-            });
+HandleCreeps();
+
+EndTick();
+
+function SetupPrototypes() {
+    Room.prototype.getLOIs = function() {
+        var lois = [];
+        this.memory.sources.forEach(function(sourceId) {
+            lois.push(sourceId);
+        });
+
+        if(this.controller) {
+            lois.push(this.controller.id);
         }
-
-        room.memory.calcComplete = true;
-    }
-});
-
-if(Object.keys(Game.creeps).length < 4) {
-    // Spawn a new creep
+        return lois;
+    };
 }
 
-Object.keys(Game.creeps).forEach(function (creepName) {
-    var creep = Game.creeps[creepName];
+function SetupRooms() {
+    Object.keys(Game.rooms).forEach(function (roomName) {
+        var room = Game.rooms[roomName];
 
-    if(creep.carry.energy < creep.carryCapacity) {
-        // Move to source or harvest
-    } else {
-        // Move to spawn or transfer
+        if (!room.memory.calcComplete || Memory.recalcRooms) {
+            // Do one time room calculations here
+
+            if(!room.memory.sources) {
+                room.memory.sources = room.find(FIND_SOURCES).map(function (sourceObject) {
+                    return sourceObject.id;
+                });
+            }
+
+            room.memory.calcComplete = true;
+        }
+    });
+}
+
+function SpawnCreeps() {
+    if(Object.keys(Game.creeps).length < 4) {
+        // Spawn a new creep
     }
-});
+}
 
-Memory.recalcRooms = false;
-console.log(Game.getUsedCpu());
+function HandleCreeps() {
+    Object.keys(Game.creeps).forEach(function (creepName) {
+        var creep = Game.creeps[creepName];
+
+        if(creep.carry.energy < creep.carryCapacity) {
+            // Move to source or harvest
+        } else {
+            // Move to spawn or transfer
+        }
+    });
+}
+
+function EndTick() {
+    Memory.recalcRooms = false;
+    console.log(Game.getUsedCpu());
+}
